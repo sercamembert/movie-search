8d8f712e1d4ae9bcdb75ef427568c0fbcefdb29b
Filip Maszota <filipmaszota3@gmail.com>
2023-05-11 19:41:46 +0200

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">
+        Movies take us on a journey beyond our everyday lives.
+      </h1>
+      <div className="container">
+        <div className="container-wrapper">
+          <div className="search"></div>
+          <section className="movie">
+            <div className="movie__container">
+              <span className="movie__title"></span>
+              <div className="movie__details-container"></div>
+              <div className="movie__genre-container"></div>
+              <span className="movie__description"></span>
+              <span className="movie__actors"></span>
+            </div>
+            <div className="movie__img"></div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
